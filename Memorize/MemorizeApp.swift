de5bbf63ae6e17af262aab799fbd4ac5cad01360
eksinyue <xinyuehehxy@gmail.com>
2020-12-21 21:10:03 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by eksinyue on 21/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
