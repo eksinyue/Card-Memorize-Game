5f798381ec4be2bd038ed207a396959bca897058
eksinyue <xinyuehehxy@gmail.com>
2020-12-21 22:26:35 +0800

    feat: add cardview (lecture 1 demo)

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack() {
+            ForEach(0..<4) { index in
+                HStack() {
+                    ForEach(0..<3) { index in
+                        CardView(isFaceUp: true)
+                    }
+                }
+            }
+        }
             .padding()
+            .foregroundColor(Color.green)
+            .font(Font.largeTitle)
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack() {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("🎄")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+ 
